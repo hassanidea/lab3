4e6d483c3ec4d2689e84b48181b98413173dfbf3
hassannhassan <hassannhassan@cb5109-19.labs.sce.carleton.ca>
2019-09-19 09:27:24 -0400

    updated

---
+++ lab1/src/lab1/AddressBook.java
@@ -0,0 +1,34 @@
+package lab1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+	private List<BuddyInfo> buddyInfos;
+	
+	public AddressBook() {
+		this.buddyInfos = new ArrayList<>();
+	}
+	
+	
+	
+	public void addBuddy(){
+		
+	}
+	
+	public void removeBuddy(){
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Address Book");
+		
+		
+	}
+
+}
